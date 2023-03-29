3e46391f6fb423e62bcc69745cc21b30e51b2054
Cameron Eldridge <cameldridge@gmail.com>
2023-03-28 22:33:33 -0400

    Wild attempt at setting up CI

--- server/config/test.exs
+++ server/config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :bale, Bale.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "bale",
+  password: "bale",
   hostname: "localhost",
   database: "bale_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
--- server/mix.exs
+++ server/mix.exs
@@ -9,7 +9,10 @@ defmodule Bale.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -40,7 +43,8 @@ defmodule Bale.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
