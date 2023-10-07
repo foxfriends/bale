6d83f92da2486f9a231fedb1f9cae29828444197
Cameron Eldridge <cameldridge@gmail.com>
2023-10-07 07:53:46 -0400

    delete everything

--- server/test/support/fixtures/avatar_fixtures.ex
+++
@@ -1,8 +0,0 @@
-defmodule BaleWeb.AvatarFixtures do
-  alias Bale.Repo
-  alias Bale.Schema.Avatar
-
-  def avatar_fixture(account_id, color \\ 0x00FF00, size \\ 50) do
-    Repo.insert(%Avatar{account_id: account_id, color: color, size: size})
-  end
-end
